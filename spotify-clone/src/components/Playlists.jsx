c4009444f7b2062ecb9d86299a6bd378b70a2247
Harveen19 <harveenkaur1912@gmail.com>
2022-12-29 15:40:08 +0530

    Add all my files

--- spotify-clone/src/components/Playlists.jsx
+++ spotify-clone/src/components/Playlists.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import { useStateProvider } from "../utils/StateProvider";
 import axios from "axios";
+import { reducerCases } from "../utils/Constants";
+
+import styled from "styled-components";
+
 export default function Playlists() {
-    const [{ token,dispatch }] = useStateProvider();
+    const [{ token, playlists },dispatch] = useStateProvider();
     useEffect(() => {
         const getPlaylistData = async () => {
             const response = await axios.get(
@@ -18,10 +22,37 @@ export default function Playlists() {
             const playlists = items.map(({ name, id }) => {
                 return { name, id };
             });
-            console.log(playlists);
-            ;
+            dispatch({ type: reducerCases.SET_PLAYLISTS, playlists });
         };
         getPlaylistData();
     }, [token,dispatch]);
-  return <div>Playlists</div>;
+    return (
+    <Container>
+        <ul>
+            {playlists.map(({ name,id })=> {
+                return <li key={id}>{name}</li>;
+            })}
+        </ul>
+    </Container>
+    );
 }
+
+const Container = styled.div`
+    ul{
+        list-style-type: none;
+        display: flex;
+        padding: 1rem;
+        gap: 1rem;
+        flex-direction: column;
+    }
+    li{
+        gap: 1rem;
+        display: flex;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+
+        &:hover{
+                color: white;
+        }
+    }
+`;
