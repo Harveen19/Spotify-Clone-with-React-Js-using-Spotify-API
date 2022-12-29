298889f84f009e5fa73e90cdbf2ee026fc9cde79
Harveen19 <harveenkaur1912@gmail.com>
2022-12-30 00:03:01 +0530

    Add all my files

--- spotify-clone/src/components/Footer.jsx
+++ spotify-clone/src/components/Footer.jsx
@@ -8,4 +8,11 @@ export default function Footer() {
 const Container = styled.div`
     background-color: #181818;
     height: 100%;
+    width: 100%;
+    display: grid;
+    border-top: 1px solid #282828;
+    align-items: center;
+    grid-template-columns: 1fr 2fr 1fr;
+    padding: 0 1rem;
+    justify-content: center;
 `;
